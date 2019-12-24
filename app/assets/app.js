dac6cfc49c5c1eb0f5d68b252fccb020b54b7fb6
Brian McGonagill <bmcgonag@gmail.com>
2019-12-24 11:45:08 -0600

    framework for project

--- app/assets/app.js
+++ app/assets/app.js
@@ -1,28 +1,7 @@
-// MIT License
-
-// Copyright (c) 2018 Neutralinojs
-
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
 let myapp = {
-    myfunction : function () { document.getElementById('info').innerHTML = NL_NAME + " is running on port " +
-                    NL_PORT + " inside " + NL_OS + "<br/><br/>" + "<span>v" + NL_VERSION + "</span>"; }
+    myfunction : function () {
+
+		}
 };
     
 
@@ -37,3 +16,27 @@ Neutralino.init({
 
     }
 });
+
+let ztapi = {
+	getNetworks : function() {
+		const networks = async () => {
+  			const response = await fetch('https://my.zerotier.com/api/network/<my user id>>', {
+    			method: 'POST',
+    			body: myBody, // string or object
+    			headers: {
+      			'Authentication': 'bearer < my user api token >'
+    			}
+  			});
+  			const myJson = await response.json(); //extract JSON from the http response
+  			// do something with myJson
+		
+		}
+	},
+	getDevices : function() {
+	
+	},
+	getUsers : function() {
+	
+	},
+	
+}
